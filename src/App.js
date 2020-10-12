01ed08077aa69f35112fe0a8ae9b5b2732e1a1af
hamouda <hamoudamdemagh@gmail.com>
2020-10-12 16:41:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import MediaCard from './components/MediaCard';
-
 import './style.css';
 import './App.css';
 
 
 
 function App() {
-  let Media = {
-    title: "Hamouda Mdemagh",
-    body:
-      "i know that only by workin so hard i'll be good at web dev",
-    
+ const Media = {
+    fullName: "Hamouda Mdemagh",
+    Bio:
+      "i'm hamouda 31 years old musician, now i've just started a new journey to become a web developper this is seems to be though for now but i know that only by working so hard i'll be good at web dev",
+      profession:"Mucian"
   };
   return (
     <div className="App">
-      
+    
     <div>
       <MediaCard
-      title={Media.title}
-      body={Media.body}
+      fullName={Media.fullName}
+      Bio={Media.Bio}
+      profession={Media.profession}
       src={Media.imageUrl}
       />
       
--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -1,41 +1,40 @@
 import React from 'react'
 import imageUrl from '../components/Assets/img.jpg'
 
-
-export default function MediaCard(props) {
-    return (
-        <div>
-            <h2>{props.title}</h2>
-            <br></br>
-        <p>{props.body}</p>
-        <br></br>
-        <img src={imageUrl} alt="Hamouda">{props.imageUrl}</img>
-        </div>
-    );
+class MediaCard extends React.Component {
+    render() {
+        
+        return (
+            <div>
+                <h2> {this.props.fullName}</h2>
+                <h3>{this.props.profession}</h3>
+        <p>{this.props.Bio}</p>
+        <img src={imageUrl} alt="Hamouda">{this.props.imageUrl}</img>
+            </div>
+        
+        )
+    }
 }
 
 
 
 
-// class MediaCard extends React.Component {
-//     render() {
-        
-//         return (
-//             <div>
-//                 <h2> {this.props.title}</h2>
-//         <p>{this.props.body}</p>
-//         <img src={this.props.imageUrl}/>
-//         {/* <img >image{this.props.image}</img> */}
-//             </div>
-        
-//         )
-//     }
+
+
+
+// function MediaCard(props) {
+//     return (
+//         <div>
+//             <h2>{props.fullName}</h2>
+//             <br></br>
+//         <p><strong>{props.Bio}</strong></p>
+//         <br></br>
+//     <p>{props.profession}</p>
+//         <img src={imageUrl} alt="Hamouda">{props.imageUrl}</img>
+//         </div>
+//     );
 // }
 
+export default MediaCard;
 
 
-// function hello(props) {
-//     return(
-//     <h1>hello {props.name}</h1>
-//     )
-// }
